d64859a7768be4a0c5aadcaa1583a2bab4086c10
Bennyrig <rigbmail@gmail.com>
2024-07-03 14:59:55 +0200

    added code to chage ip and port in cmd line

--- Assets/Scripts/FIshnet_scripts/Server_Client_Script.cs
+++ Assets/Scripts/FIshnet_scripts/Server_Client_Script.cs
@@ -7,94 +7,173 @@ using System.Collections;
 
 public class NetworkHelper : MonoBehaviour
  {
-    private NetworkManager _networkManager;
+    private NetworkManager networkManager;
     public bool startServerInEditor = false;
     public bool startClientInEditor = false;
     public string serverIP = "localhost";
     public int serverPort = 7777;  // Add server port variable
 
-
     void Start()
     {
-        _networkManager = FindObjectOfType<NetworkManager>();
+        networkManager = FindObjectOfType<NetworkManager>();
 
-        if (_networkManager == null)
+        if (networkManager == null)
         {
             Debug.LogError("Network Manager is not assigned!");
             return;
         }
 
-        #if UNITY_EDITOR  
-        if (startServerInEditor)
+        if (Application.isEditor)
         {
-            _networkManager.ServerManager.StartConnection();
-        }
-
-        if(startClientInEditor){
-            _networkManager.ClientManager.StartConnection();
-        }
-
-        #else
-        string[] args = System.Environment.GetCommandLineArgs();
-        for(int i = 0; i < args.Length-1;i++)
-        {
-            if (args[i] == "-closeafter"){
-                int time;
-                if (!int.TryParse(args[i+1],  out time)){
-                    Debug.LogError("Unable to parse string.");    
-                }
-                StartCoroutine(waiter(time));
-                break;
-            }
-        }
-
-        args = System.Environment.GetCommandLineArgs();
-
-        foreach (string arg in args)
-        {
-            if (arg == "-server")
+            if (startServerInEditor)
             {
-                _networkManager.ServerManager.StartConnection();
+                networkManager.ServerManager.StartConnection();
                 return;
             }
-            else if (arg == "-client")
-            {
-                _networkManager.ClientManager.StartConnection();
+
+            if(startClientInEditor){
+                networkManager.ClientManager.StartConnection();
                 return;
             }
-            else if (arg == "-server_ip")
-            {
-                int index = System.Array.IndexOf(args, arg);
-                if (index < args.Length - 1)
-                {
-                    serverIP = args[index + 1];
-                }
-                break;
-            }
-            else if(arg == "-server_port")  // Handle server port argument
-            {
-                int index = System.Array.IndexOf(args, arg);
-                if (index < args.Length - 1)
-                {
-                    serverPort = int.Parse(args[index + 1]);
-                }
-                break;
-            }
         }
+        else{
+            string[] args = System.Environment.GetCommandLineArgs();
+            for(int i = 0; i < args.Length-1;i++)
+            {
+                if (args[i] == "-closeafter"){
+                    int time;
+                    if (!int.TryParse(args[i+1],  out time)){
+                        Debug.LogError("Unable to parse string.");    
+                    }
+                    StartCoroutine(waiter(time));
+                    break;
+                }
+            }
+            args = System.Environment.GetCommandLineArgs();
+            foreach (string arg in args)
+            {
+                if (arg == "-ip")
+                {
+                    int index = System.Array.IndexOf(args, arg);
+                    if (index < args.Length - 1)
+                    {
+                        serverIP = args[index + 1];
+                    }
+                }
+                else if(arg == "-port")  // Handle server port argument
+                {
+                    int index = System.Array.IndexOf(args, arg);
+                    if (index < args.Length - 1)
+                    {
+                        serverPort = int.Parse(args[index + 1]);
+                    }
+                }
+            }
 
-        // // Set the server IP
-        // networkManager.networkAddress = serverIP;
+            // // Set the server IP
+            networkManager.TransportManager.Transport.SetClientAddress(serverIP);
+            networkManager.TransportManager.Transport.SetPort((ushort)serverPort);
 
-        // // Set the server port if a port transport is available
-        // if (Transport.active is PortTransport portTransport)
-        // {
-        //     portTransport.Port = (ushort)serverPort;
-        // }
-        
-        // networkManager.StartClient();
-        #endif
+            args = System.Environment.GetCommandLineArgs();
+            foreach (string arg in args)
+            {
+                if (arg == "-server")
+                {
+                    networkManager.ServerManager.StartConnection();
+                    return;
+                }
+                else if (arg == "-client")
+                {
+                    networkManager.ClientManager.StartConnection();
+                    return;
+                }
+            }
+
+        }
 
     }
+
+    // void Start()
+    // {
+    //     _networkManager = FindObjectOfType<NetworkManager>();
+
+    //     if (_networkManager == null)
+    //     {
+    //         Debug.LogError("Network Manager is not assigned!");
+    //         return;
+    //     }
+
+    //     #if UNITY_EDITOR  
+    //     if (startServerInEditor)
+    //     {
+    //         _networkManager.ServerManager.StartConnection();
+    //     }
+
+    //     if(startClientInEditor){
+    //         _networkManager.ClientManager.StartConnection();
+    //     }
+
+    //     #else
+    //     string[] args = System.Environment.GetCommandLineArgs();
+    //     for(int i = 0; i < args.Length-1;i++)
+    //     {
+    //         if (args[i] == "-closeafter"){
+    //             int time;
+    //             if (!int.TryParse(args[i+1],  out time)){
+    //                 Debug.LogError("Unable to parse string.");    
+    //             }
+    //             StartCoroutine(waiter(time));
+    //             break;
+    //         }
+    //     }
+
+    //     args = System.Environment.GetCommandLineArgs();
+
+    //     foreach (string arg in args)
+    //     {
+    //         if (arg == "-server")
+    //         {
+    //             _networkManager.ServerManager.StartConnection();
+    //             return;
+    //         }
+    //         else if (arg == "-client")
+    //         {
+    //             _networkManager.ClientManager.StartConnection();
+    //             return;
+    //         }
+    //         else if (arg == "-server_ip")
+    //         {
+    //             int index = System.Array.IndexOf(args, arg);
+    //             if (index < args.Length - 1)
+    //             {
+    //                 serverIP = args[index + 1];
+    //             }
+    //             break;
+    //         }
+    //         else if(arg == "-server_port")  // Handle server port argument
+    //         {
+    //             int index = System.Array.IndexOf(args, arg);
+    //             if (index < args.Length - 1)
+    //             {
+    //                 serverPort = int.Parse(args[index + 1]);
+    //             }
+    //             break;
+    //         }
+    //     }
+
+    //     // // Set the server IP
+    //     // networkManager.networkAddress = serverIP;
+
+    //     // // Set the server port if a port transport is available
+    //     // if (Transport.active is PortTransport portTransport)
+    //     // {
+    //     //     portTransport.Port = (ushort)serverPort;
+    //     // }
+        
+    //     // networkManager.StartClient();
+    //     #endif
+
+    // }
     IEnumerator waiter(int time)
     {
         yield return new WaitForSeconds(time);
